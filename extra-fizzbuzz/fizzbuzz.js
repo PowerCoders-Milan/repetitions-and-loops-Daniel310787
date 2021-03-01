3a64b591708f4d982e9e5580471cac1a6ae9a28a
Daniel Alvarado <Daniel Alvarado daniel310787@hotmail.com>
2021-03-01 16:13:03 +0100

    adding last exercise

---
+++ extra-fizzbuzz/fizzbuzz.js
@@ -0,0 +1,43 @@
+/*## Multiplication Tables
+
+Write a JavaScript program which iterates the integers from 1 to 100 and returns Fizz or Buzz for certain numbers.
+
+#### instructions
+1. For multiples of three print "Fizz", instead of the number and for the multiples of five print "Buzz".
+2. For numbers which are multiples of both three and five print "FizzBuzz"*/
+
+
+for(var i = 1; i <= 100; i++){
+     
+    if(i % 3 == 0 && i % 5 == 0){
+        console.log("FizzBuzz")
+    }else if(i % 3 == 0){
+        console.log("Fizz")
+    } else if (i % 5 == 0){
+        console.log("Buzz")
+    } else if(i % 3 > 0 && i % 5 > 0){
+        console.log(i)
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
