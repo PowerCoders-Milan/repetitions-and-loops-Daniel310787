61b17834f33704bdcbdcb383a60eb9118bf6dc85
Daniel Alvarado <Daniel Alvarado daniel310787@hotmail.com>
2021-03-01 12:44:12 +0100

    adding 3rd exercise

--- 03-Multiplication-Tables/multiplication_tables.js
+++ 03-Multiplication-Tables/multiplication_tables.js
@@ -1,1 +1,18 @@
 // write your code below
+
+for(var num = 0; num <= 10; num++) {
+    var multiply = num * 9
+    console.log(num+ "* 9 = "+multiply)
+}
+
+//extra
+
+function number (num, n) {
+    var num = prompt("Can I get any number from 0-10")
+    for(var n = 0; n <= 10; n++){
+        var result = num * n;
+        window.alert(num + " * " + n + " = " + result)
+    }
+}
+
+number()
